35ba05d047e1d6987eaae5562eabf666c5f51f0d
Andrew Roberts <andrew.roberts@newellco.com>
2020-03-18 15:16:21 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FluentInterfaceDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var spaceship = Spaceship.Create().WithName("Saturn V").OfSize(100, 100).WithThrusters();
+            var spaceship2 = Spaceship.Create().WithName("Teeny Rocket").OfSmallSize().WithThrusters();
+            Console.WriteLine(spaceship);
+            Console.WriteLine();
+            Console.WriteLine(spaceship2);
+        }
+    }
+}
---
+++ Spaceship.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace FluentInterfaceDemo
+{
+    public class Spaceship
+    {
+        public string Name;
+        public int Width;
+        public int Height;
+        public bool HasThrusters;
+
+        private Spaceship()
+        {
+        }
+
+        public static Spaceship Create()
+        {
+            return new Spaceship() { Name = "My spaceship", Width = 100, Height = 100 };
+        }
+
+        public Spaceship WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        public Spaceship OfSize(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            return this;
+        }
+
+        public Spaceship WithThrusters()
+        {
+            HasThrusters = true;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Spaceship\nName: {0}\nWidth: {1}\nHeight: {2}\nThrusters: {3}", Name, Width, Height, HasThrusters);
+        }
+    }
+
+    public static class Spaceships
+    {
+        public static Spaceship OfSmallSize(this Spaceship spaceship)
+        {
+            spaceship.Width = 10;
+            spaceship.Height = 10;
+            return spaceship;
+        }
+
+        public static Spaceship OfMediumSize(this Spaceship spaceship)
+        {
+            spaceship.Width = 100;
+            spaceship.Height = 100;
+            return spaceship;
+        }
+
+        public static Spaceship OfLargeSize(this Spaceship spaceship)
+        {
+            spaceship.Width = 1000;
+            spaceship.Height = 1000;
+            return spaceship;
+        }
+    }
+}
